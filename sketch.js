94fcfec8c56623c16df747b452f30f7f8f7af352
GCIS120049 <aaravika2@gmail.com>
2021-02-16 15:58:56 +0530

    changes

--- sketch.js
+++ sketch.js
@@ -1,29 +1,48 @@
-var ball;
+var car;
+var database,pos;
 
 function setup(){
+    database=firebase.database();
+
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+
+    car = createSprite(250,250,10,10);
+    car.shapeColor = "red";
+    var carps=database.ref('ball/position');
+    carps.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
+    if(pos!==undefined){
+        if(keyDown(LEFT_ARROW)){
+            changePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            changePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            changePosition(0,-1);
+     }
+        else if(keyDown(DOWN_ARROW)){
+            changePosition(0,+1);
+     }
+        drawSprites();
+   }
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({
+        'x':pos.x+x,
+        'y':pos.y+y
+    });
+    }
+
+function readPosition(data){
+    pos=data.val();
+    car.x=pos.x;
+    car.y=pos.y;
+}
+function showError(){
+    console.log("error");
 }
